6aff20d0a50b774a721711a4361aea7658332b7e
philipcori <philip.cori@gmail.com>
2020-03-10 13:32:25 -0700

    implement experiment

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import librosa
+import os
 
 def main():
-    print('hello')
+    data_dir = os.path.join(os.getcwd(), 'data\\')
+    data_files = [os.path.join(data_dir, f) for f in os.listdir(data_dir)]
+    y, sr = librosa.load(data_files[0])
+
+    # y, sr = librosa.load(librosa.util.example_audio_file())
+    notes = librosa.onset.onset_detect(y=y, sr=sr)
+    notes = librosa.frames_to_time(notes, sr=sr)
+    oenv = librosa.onset.onset_strength(y, sr=sr)
+    # tempogram = librosa.feature.tempogram(onset_envelope=oenv, sr=sr, hop_length=hop_length)
+    # ac_global = librosa.autocorrelate(oenv, max_size=tempogram.shape[0])
+    # ac_global = librosa.util.normalize(ac_global)
+    # # Estimate the global tempo for display purposes
+    # tempo = librosa.beat.tempo(onset_envelope=oenv, sr=sr, hop_length=hop_length)[0]
+    x = range(notes.shape[0])
+    y = [x if (x > 10) else 0 for x in notes]
+
+    tempogram = librosa.feature.tempogram(y, sr)
+
+    print(tempogram.shape)
+
 
 
 if __name__ == '__main__':
